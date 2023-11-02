4ccef53443ef0dc9ab296e95827bbda20c9414c3
254jack <taixonj@gmail.com>
2023-11-02 11:31:47 +0300

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -46,10 +46,15 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
-int binary_tree_left_depth(const binary_tree_t *tree)
-
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+binary_tree_t *binary_search_uncle(binary_tree_t *node);
+size_t balance_height(const binary_tree_t *tree);
+size_t recursive_height(const binary_tree_t *tree);
+
+
+/* Print function */
+void binary_tree_print(const binary_tree_t *);
 #endif /* _BINARY_TREES_H_ */
